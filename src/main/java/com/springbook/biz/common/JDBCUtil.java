df2467c87d44cab52c8e143b6e2e67189e9ee7c2
owner0gn <owner0gn@gmail.com>
2019-02-13 14:57:33 +0900

    JDBC connection test

--- src/main/java/com/springbook/biz/common/JDBCUtil.java
+++ src/main/java/com/springbook/biz/common/JDBCUtil.java
@@ -3,12 +3,13 @@ package com.springbook.biz.common;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class JDBCUtil {
 	public static Connection getConnection() {
 		try {
 			Class.forName("org.h2.Driver");
-			return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/test", "sa", "");
+			return DriverManager.getConnection("jdbc:h2:tcp://localhost/~/boardWeb", "sa", "");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -40,4 +41,43 @@ public class JDBCUtil {
 			}
 		}
 	}
+	
+	public static void close(ResultSet rs, PreparedStatement stmt, Connection conn) {
+		if(rs != null) {
+			try {
+				if(!rs.isClosed()) {
+					rs.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				rs = null;
+			}
+		}
+		
+		if(stmt != null) {
+			try {
+				if(stmt.isClosed()) {
+					stmt.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				stmt = null;
+			}
+		}
+		
+		if(conn != null) {
+			try {
+				if(conn.isClosed()) {
+					conn.close();
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				conn = null;
+			}
+		}
+	}
+	
 }
